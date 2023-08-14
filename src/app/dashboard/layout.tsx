3669fd2e21fb8d7cb7161b583e7e1cb3ca7dae2a
ximena.paparella@karvi.com <ximenapaparella@gmail.com>
2023-08-14 09:08:53 -0300

    Improve Layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import '../../app/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import {NavMenu} from '@/components/navMenu' 
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
+      <main className="flex min-h-screen flex-row p-2 w-full">
+      <div className="flex-none w-2/12 h-14 p-4">
+        <NavMenu/>
+      </div>
+      <div className="flex-initial w-10/12 p-4 border-slate-100">
+      {children}
+      </div>
+    </main>
       </body>
     </html>
   )
--- src/app/dashboard/treatments/page.tsx
+++ src/app/dashboard/treatments/page.tsx
@@ -3,13 +3,8 @@ import {NavMenu} from '@/components/navMenu'
 
 export default function  ListTreatments() {
   return (
-    <main className="flex min-h-screen flex-row p-2 w-full">
-      <div className="flex-none w-2/12 h-14 p-4">
-        <NavMenu/>
-      </div>
       <div className="flex-initial w-10/12 p-4 border-slate-100">
        Listar tratamientos
       </div>
-    </main>
   )
 }
