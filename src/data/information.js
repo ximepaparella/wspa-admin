154317a210f07aff9e48171e37aa25d991a8408e
ximena.paparella@karvi.com <ximenapaparella@gmail.com>
2023-08-14 08:52:44 -0300

    Build menu

---
+++ src/data/information.js
@@ -0,0 +1,17 @@
+const information = [ {
+    address: 'Avenida del puerto 240, Bahía Grande, Nordelta',
+    whatsapp: '+5491134209650',
+    times: {
+        monday: 'Closed',
+        tuesday: '11 a 20hs',
+        wednesday: '10 a 21hs',
+        thursday: '10 a 21hs',
+        friday: '10 a 21hs',
+        saturday: '10 a 21hs',
+        sunday: '10 a 21hs',
+
+    }
+}
+  
+    
+]
