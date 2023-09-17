56ead7e40413a859cc74d2b3223e5913300b4c64
ximena.paparella@karvi.com <ximenapaparella@gmail.com>
2023-09-17 19:27:15 -0300

    Reset proyect

--- src/app/dashboard/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default function Settings() {
-  return (
-      <div className="flex-initial w-10/12 p-4 border-slate-100">
-       Congfigraciones
-      </div>
-  )
-}
--- src/app/dashboard/treatments/add/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default function AddTreatment() {
-  return (
-      <div className="flex-initial w-10/12 p-4 border-slate-100">
-       Añadir tratamiento
-      </div>
-  )
-}
